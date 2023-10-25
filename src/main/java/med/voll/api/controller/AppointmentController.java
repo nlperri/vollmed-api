f5591ec2495ea81122bdc22a2ea00f0c55b512e2
natalia <lnataliaperri@gmail.com>
2023-10-25 09:32:43 -0300

    feat: add spring doc configurations

--- src/main/java/med/voll/api/controller/AppointmentController.java
+++ src/main/java/med/voll/api/controller/AppointmentController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import med.voll.api.domain.appointment.*;
 import org.apache.coyote.Response;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("appointments")
+@SecurityRequirement(name = "bearer-key")
 public class AppointmentController {
 
     @Autowired
